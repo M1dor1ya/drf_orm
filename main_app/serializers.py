1e9d561d532729f254b3f5354d1b4e34740dca18
Zcs <772987650@qq.com>
2019-05-10 17:55:33 +0800

    update

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -12,9 +12,12 @@ class GraphSerializer(serializers.ModelSerializer):
     class Meta:
         model = Graph
         fields = '__all__'
-        read_only_fields = ['graph_content', 'graph_conf', 'graph_position', 'create_time']
-        #  read_only_fields选项，表示该字段只用作查询出结果，不需要用户传值
+        read_only_fields = ['graph_name', 'graph_content', 'graph_conf', 'graph_position', 'create_time']
+        #  read_only_fields选项，表示该字段只用作查询出结果，不允许用户传值
         #  如果未设置该字段，那么表示所有字段都需要从serializer那传值，如果未传is_valid函数会返回False
+
+        #  extra_kwargs = {'id': {'write_only': True}}
+        #  提供extra_kwargs的方式来为某个字段指定其关键字参数，而不需要去显示指定(id=serializers.IntegerField())
 
     @staticmethod
     def create_graph(gspace, gtime, num):
@@ -31,8 +34,8 @@ class GraphSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         print(validated_data)
-        validated_data['graph_conf'] = str(validated_data['upper_limit']) + '|' + \
-                                       str(validated_data['lower_limit']) + '|' + str(validated_data['size'])
+        validated_data['graph_conf'] = str(validated_data['lower_limit']) + '|' + \
+                                       str(validated_data['upper_limit']) + '|' + str(validated_data['size'])
         validated_data['graph_position'] = 0
         validated_data['graph_content'] = str(self.create_graph([1,4], [validated_data['lower_limit'],validated_data['upper_limit']],10))
         kwargs = {
@@ -43,4 +46,10 @@ class GraphSerializer(serializers.ModelSerializer):
             'graph_position': validated_data.get('graph_position'),
         }
         instance = Graph.objects.create(**kwargs)
+        return instance
+
+    def update(self, instance, validated_data):
+        #  只允许更新graph_status参数
+        instance.graph_status = validated_data['graph_status']
+        instance.save()
         return instance
--- main_app/views.py
+++ main_app/views.py
@@ -33,23 +33,36 @@ class GraphView(APIView):
         stream = BytesIO(request.body)  # 将JSON数据转换为流形式
         data = JSONParser().parse(stream)  # 解析流中的JSON数据并转换为Python数据结构
         serializer = GraphSerializer(data=data)
-
         if serializer.is_valid():  # 验证前台传过来的数据是否合法，在save前必须调用该方法
-            serializer.save(**data)  # 根据是否存在instance实例来决定执行create方法或update方法
+            serializer.save(**data)  # 根据是否存在instance实例来决定执行create方法或update方法，无执行create
         #  save方法可以选择传参或者不传参，不传参的话默认会从serializer实例化时传入的数据进行读取
         #  但是这样读取的话，非model定义的字段会被舍弃掉，所以如果需要传入非model定义的字段，需要在save方法传入data
             return 0
 
         #  print(serializer.errors)
 
-
     def patch(self, request):
         stream = BytesIO(request.body)
         data = JSONParser().parse(stream)
         serializer = GraphSerializer(data=data)
+        serializer.instance = Graph.objects.get(id=data['id'])
         if serializer.is_valid():
             serializer.save(**data)
         return 0
 
     def delete(self, request):
-        pass
+        """
+        RESTful默认界面的DELETE方法会将URL中的id传递给后台，如果没有使用URL传参的话，是不会传过来数据的
+        自己传递JSON过来使用以下方法解析即可
+        :param request:
+        :return:
+        """
+        stream = BytesIO(request.body)
+        data = JSONParser().parse(stream)
+        print(data)
+        print(type(data))
+        g_id = data['id']
+        print(g_id)
+        obj = Graph.objects.get(id=g_id)
+        obj.delete()
+        return Response({"status": 0})
